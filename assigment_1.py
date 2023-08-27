451bee0d528401f8187f650c9b726773fcf3724a
Mohammad Yamin <107704708+yaminyann@users.noreply.github.com>
2023-08-27 22:27:03 +0600

    Add files via upload

---
+++ assigment_1.py
@@ -0,0 +1,74 @@
+# Assignment -  1
+# print your name
+print('MD YAMIN ALI')
+
+
+# Assignment - 2
+# input your batch number using input() function
+Batch = int(input('Enter Your Batch Number : '))
+print('Your Batch number is BATCH:',Batch)
+
+
+# Assignment - 3
+# create 5 diffarent valid variable and print
+Name = 'MD YAMIN ALI'
+date_Of_Birth = '15/11/2003'
+age,year = 18,2023
+nickname2 = 'YANN'
+sscResult = hscResult = 4.50
+
+print('My name is : ',Name)
+print('My Date of birth : ',date_Of_Birth)
+print('My age is : ',age,'. And now Year is : ',year)
+print('My nickname is : ',nickname2)
+print('My SSC result and HSC result is : SSC: ',sscResult,'HSC: ',hscResult)
+
+
+# Assignment - 4
+# create pascal case variable
+MyHomeAddress = 'Chuadanga,Khulna,Bangladesh'
+print('Print my home address using pascel case variabel . My Home address is : ',MyHomeAddress)
+
+
+# Assignment - 5
+# assign a single value to multiple variable
+year = Cureent_year = thisYear = is_year = 2023
+print('Assign a single value is multiple variable . Year is :',year,'current year is :',Cureent_year,'This year is',thisYear,"and year is ",is_year)
+
+
+# Assignment - 6
+# input float value using input()
+floatValue = float(input('Enter a Float value like PI : '))
+print('Your float value is like PI : ',floatValue)
+
+
+# Assignment - 7
+# convert into uppercase
+a = 'Welcome to Django web and Ai'
+print(a.upper())
+
+
+# Assihnment - 8
+# remove whitespace
+b = '        python'
+print(b.strip())
+
+
+# Assignment - 9
+# convert int to float
+c = 60
+d = 22
+print('Convert INT to FLOAT : ',float(c))
+print('Convert INT to FLOAT : ',float(d))
+
+
+
+# Assignment - 10
+# create dictonary and print your info
+MyInfo = {
+    'MyName' :'Yamin',
+    'age' : 18,
+    'collage' : 'Magura polytecnic institute',
+    'department' : 'cst - computer scince and technology'
+}
+print(MyInfo)
